5e07bc1fbfbf5ee7440e8ff19ac3bdb1c12fc8b7
Illidanz <illidanrulez@gmail.com>
2020-04-26 17:59:34 +0200

    Change default width/height to 256

--- ndstextgen/__init__.py
+++ ndstextgen/__init__.py
@@ -1,4 +1,4 @@
-def gen(font, text, out="text.png", vert=2, fw=0, color="black", bg="transparent", width=512, height=512, no_crop=False):
+def gen(font, text, out="text.png", vert=2, fw=0, color="black", bg="transparent", width=256, height=256, no_crop=False):
     from . import cli
     args = [font, text, "--out", out, "--vert", vert, "--fw", fw, "--color", color, "--bg", bg, "--width", width, "--height", height]
     if no_crop:
